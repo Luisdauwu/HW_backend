020abbc0963fa7f554cd92d8293169d5bb5a5a21
LuisCarrillo <142847477+Luisdauwu@users.noreply.github.com>
2024-05-19 01:38:01 -0500

    Delete BACKEND directory

--- BACKEND/models/user_models.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class user(BaseModel):
-    username: str
-    password: str
-   
--- BACKEND/schema/user_schema.py
+++
@@ -1,7 +0,0 @@
-def car_serializer(user)->dict:
-    return {
-        "username": user["username"],
-        "password": user["password"],
-        
-    }
-
